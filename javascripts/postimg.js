c227ab19aae6f75e4944353f1e06c62b2d854d7a
projectivity <Student@MAC26.local>
2016-08-02 13:31:57 -0400

    Lightbox toggle unneeded when group present

--- javascripts/postimg.js
+++ javascripts/postimg.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	var i;
 	for (i = 0; i < imageList.length; i++)
 	{
-		imageList[i].setAttribute( 'data-jslghtbx' );
 		imageList[i].setAttribute( 'data-jslghtbx-group', 'post' );
 		imageList[i].className += " jslghtbx-thmb";
 	}
